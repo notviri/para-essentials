c36ffdb709cf93dde6ad450e64a5f5601e4aa469
viri <git@stuff.moe>
2020-08-15 17:39:45 -0600

    add /help command

--- src/moe/stuff/para/ParaEssentials.java
+++ src/moe/stuff/para/ParaEssentials.java
@@ -1,14 +1,24 @@
 package moe.stuff.para;
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import moe.stuff.para.commands.CommandHelp;
 import moe.stuff.para.commands.CommandKill;
 
 public class ParaEssentials extends JavaPlugin {
+    static final String CONFIG_FILENAME = "config.yml";
+
+    public FileConfiguration config;
+
     CommandKill commandKill;
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        this.reloadConfig();
         this.setup(false);
     }
 
@@ -18,6 +28,14 @@ public class ParaEssentials extends JavaPlugin {
     }
 
     public void setup(boolean reload) {
+        this.getCommand("help").setExecutor(new CommandHelp(this));
         this.getCommand("kill").setExecutor(new CommandKill(this));
+    }
+
+    @Override
+    public void reloadConfig() {
+        File configFile = new File(this.getDataFolder(), CONFIG_FILENAME);
+        YamlConfiguration yamlConfig = YamlConfiguration.loadConfiguration(configFile);
+        this.config = yamlConfig;
     }
 }
---
+++ src/moe/stuff/para/commands/CommandHelp.java
@@ -0,0 +1,58 @@
+package moe.stuff.para.commands;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import moe.stuff.para.ParaEssentials;
+
+public class CommandHelp implements CommandExecutor {
+    class CommandInfo {
+        String usage, desc;
+    }
+
+    ParaEssentials pluginInstance;
+    String[] helpMessage;
+
+    public CommandHelp(ParaEssentials pluginInstance) {
+        this.pluginInstance = pluginInstance;
+
+        String sepLines = "---------------------------------------------";
+        String separator = "" + ChatColor.GOLD + ChatColor.BOLD + sepLines;
+
+        // we cache the help message here
+        ArrayList<String> helpMessage = new ArrayList<>();
+        helpMessage.add(separator);
+        Map<String, Object> commands = pluginInstance.config
+                .getConfigurationSection("help")
+                .getValues(true);
+        TreeMap<String, CommandInfo> commandInfo = new TreeMap<>();
+        for (Map.Entry<String, Object> entry : commands.entrySet()) {
+            if (!(entry.getKey().contains("."))) continue;
+            String[] split = entry.getKey().split("\\.");
+            commandInfo.putIfAbsent(split[0], new CommandInfo());
+            CommandInfo info = commandInfo.get(split[0]);
+            if (split[1].equals("usage")) {
+                info.usage = entry.getValue().toString().replace("<command>", split[0]);
+            } else if (split[1].equals("desc")) {
+                info.desc = entry.getValue().toString();
+            }
+        }
+        for (CommandInfo info : commandInfo.values()) {
+            helpMessage.add("" + ChatColor.GOLD + info.usage + ": " + ChatColor.WHITE + info.desc);
+        }
+        helpMessage.add(separator);
+        this.helpMessage = helpMessage.toArray(new String[helpMessage.size()]);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(this.helpMessage);
+        return true;
+    }
+}
