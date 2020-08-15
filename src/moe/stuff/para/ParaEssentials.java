08c25f0f9ab656cd1e45ef10bdf6ed65805bf2a0
viri <git@stuff.moe>
2020-08-15 15:17:43 -0600

    setup project

---
+++ src/moe/stuff/para/ParaEssentials.java
@@ -0,0 +1,13 @@
+package moe.stuff.para;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ParaEssentials extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO
+    }
+    @Override
+    public void onDisable() {
+        // TODO
+    }
+}
