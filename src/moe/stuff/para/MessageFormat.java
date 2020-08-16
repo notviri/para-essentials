32908666a075560f26500ab51e38d5450c1b1f39
viri <git@stuff.moe>
2020-08-15 18:20:51 -0600

    update message formatting syntax

--- src/moe/stuff/para/MessageFormat.java
+++ src/moe/stuff/para/MessageFormat.java
@@ -2,6 +2,6 @@ package moe.stuff.para;
 
 public class MessageFormat {
     public static String parseFormat(String s) {
-        return s.replaceAll("%", "%%").replaceAll("NAME", "%s").replaceAll("MSG", "%s");
+        return s.replaceAll("%", "%%").replaceAll("\\$", "§").replaceAll("NAME", "%s").replaceAll("MSG", "%s");
     }
 }
