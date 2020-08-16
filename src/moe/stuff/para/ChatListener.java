2fbea8cc37b180798244e778dc00edaa2e2621da
viri <git@stuff.moe>
2020-08-16 12:18:37 -0600

    implement death message toggle

--- src/moe/stuff/para/ChatListener.java
+++ src/moe/stuff/para/ChatListener.java
@@ -4,13 +4,19 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.server.BroadcastMessageEvent;
 
 public class ChatListener implements Listener {
+    public static final String DEATH_PREFIX = "[death] ";
+
     ParaEssentials pluginInstance;
     String chatFormat;
 
@@ -22,6 +28,35 @@ public class ChatListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         event.setFormat(this.chatFormat);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Bukkit.broadcastMessage(DEATH_PREFIX + event.getDeathMessage());
+        event.setDeathMessage(null);
+    }
+
+    @EventHandler
+    public void onBroadcast(BroadcastMessageEvent event) {
+        if (!event.getMessage().startsWith(DEATH_PREFIX)) return;
+
+        event.setMessage(event.getMessage().replace(DEATH_PREFIX, ""));
+        ConcurrentHashMap<String, ChatSettings> chatSettings = this.pluginInstance.chatSettings;
+        Set<CommandSender> recipients = event.getRecipients();
+        Iterator<CommandSender> iter = recipients.iterator();
+        while (iter.hasNext()) {
+            CommandSender target = iter.next();
+            if (target instanceof Player) {
+                Player player = (Player)target;
+                ChatSettings settings = chatSettings.get(player.getName());
+                if (settings == null) continue;
+                synchronized (settings) {
+                    if (settings.areDeathMessagesDisabled()) {
+                        iter.remove();
+                    }
+                }
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
